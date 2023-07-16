e6e3fb69edb91c5851b31efca2dbfca8dcaaaccc
IvanProProgger <i.shelukhin@internet.ru>
2023-07-16 10:43:22 +0300

    second commit

--- conftest.py
+++ conftest.py
@@ -1,39 +1,33 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-
-
-@pytest.fixture(scope="function")
-def browser():
-    print("\nstart browser for test..")
-    browser = webdriver.Chrome()
-    yield browser
-    print("\nquit browser..")
-    browser.quit()
-
-
-supported_browsers = {
-    'chrome': webdriver.Chrome,
-    'firefox': webdriver.Firefox
-}
+from selenium.webdriver import FirefoxProfile
+from selenium.webdriver.chrome.options import Options
 
 
 def pytest_addoption(parser):
     parser.addoption('--browser_name', action='store', default='chrome',
                      help="Choose browser: chrome or firefox")
+    parser.addoption('--language', action='store', default='en',
+                     help="Choose language")
 
 
 @pytest.fixture(scope="function")
 def browser(request):
     browser_name = request.config.getoption("browser_name")
-
-    if browser_name in supported_browsers:
-        browser = supported_browsers.get(browser_name)()
-        print(f"\nstart {browser_name} browser for test..")
+    user_language = request.config.getoption("language")
+    if browser_name == "chrome":
+        print("\nstart chrome browser for test..")
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+        browser = webdriver.Chrome(options=options)
+    elif browser_name == "firefox":
+        print("\nstart firefox browser for test..")
+        options = FirefoxProfile()
+        options.set_preference("intl.accept_languages", user_language)
+        options.binary_location = r"C:\Program Files\Mozilla Firefox\firefox.exe"
+        browser = webdriver.Firefox()
     else:
-        joined_browsers = ', '.join(supported_browsers.keys())
-        raise pytest.UsageError(f"--browser_name is invalid, supported browsers: {joined_browsers}")
-
+        print(f"{browser_name} wasn't implemented")
     yield browser
     print("\nquit browser..")
     browser.quit()
